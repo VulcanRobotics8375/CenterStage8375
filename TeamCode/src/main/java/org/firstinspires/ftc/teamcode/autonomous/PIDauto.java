8f4d0116899d3595e9613d8391aab76c2afdb0e3
SheiakahEye123 <86978045+SheiakahEye123@users.noreply.github.com>
2024-02-25 10:07:13 -0800

    pidauto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PIDauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/PIDauto.java
@@ -2,15 +2,22 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.robot.MainConfig;
 import org.firstinspires.ftc.teamcode.robotcorelib.math.control.PID;
+import org.firstinspires.ftc.teamcode.robotcorelib.math.utils.MathUtils;
 import org.firstinspires.ftc.teamcode.robotcorelib.motion.followers.ParametricGuidingVectorField;
+import org.firstinspires.ftc.teamcode.robotcorelib.motion.followers.PurePursuit;
+import org.firstinspires.ftc.teamcode.robotcorelib.motion.path.Path;
+import org.firstinspires.ftc.teamcode.robotcorelib.motion.path.PathBuilder;
 import org.firstinspires.ftc.teamcode.robotcorelib.opmode.AutoPipeline;
+import org.firstinspires.ftc.teamcode.robotcorelib.robot.Robot;
 import org.firstinspires.ftc.teamcode.robotcorelib.util.RobotRunMode;
 import org.firstinspires.ftc.teamcode.robotcorelib.util.Toggle;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.vision.apriltag.AprilTagLight;
 import org.opencv.core.Scalar;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -23,8 +30,28 @@ public class PIDauto extends AutoPipeline {
     ElapsedTime timer = new ElapsedTime();
     private Toggle redToggle = new Toggle(), backDropToggle = new Toggle(), parkToggle = new Toggle();
     boolean red;
-    PID pid = new PID(0,0,0);
+    PID pidX = new PID(0,0,0);
+    PID pidT = new PID(0,0,0);
+    PID pidY = new PID(0,0,0);
     AprilTagLight pipeline;
+    double S=0.65;
+    double ALLOWED_POSE_ERROR = 1;
+    double ALLOWED_HEADING_ERROR = 3;
+
+    Pose2d robotPose;
+    Pose2d robotVelocity;
+    double poseError;
+    double headingError;
+
+    double speed;
+    double turnSpeed;
+
+    final double POSE_ERROR_GAIN = 1.00001;
+    final double HEADING_ERROR_GAIN = 1.0001;
+
+
+
+    PurePursuit purePursuit = new PurePursuit();
 
     public void runOpMode() {
         cameraInit();
@@ -37,14 +64,27 @@ public class PIDauto extends AutoPipeline {
                 pipeline.red = !pipeline.red;
             }
         }
-        Pose2d robotPose = null;
-        while (robotPose.position.y <= 40) {
-            subsystems.drivetrain.mecanumDrive(1, 0, 0);
-        }
-        double tHeading = i*10 + 30
-        while (Math.abs(subsystems.drivetrain.getIMU().getAngularOrientation().firstAngle - 30) ) {
-        }
 
+        follow(new Pose2d(), subsystems.drivetrain);
+
+
+
+    }
+
+    private void follow(Pose2d pose, Drivetrain dt) {
+        while((poseError > ALLOWED_POSE_ERROR || headingError > Math.toRadians(ALLOWED_HEADING_ERROR))) {
+            Robot.update();
+            robotPose = Robot.getRobotPose();
+            robotVelocity = Robot.getRobotVelocity();
+            poseError = Math.hypot(pose.getX() - robotPose.getX(), pose.getY() -  robotPose.getY());
+            headingError = MathUtils.calcAngularError(pose.getHeading(), robotPose.getHeading());
+            speed = poseError * POSE_ERROR_GAIN; //pose gain
+            turnSpeed = headingError * HEADING_ERROR_GAIN; //heading gain
+            double idealX = Math.sin(pose.getHeading())*speed;
+            double idealY = Math.cos(pose.getHeading())*speed;
+
+            dt.mecanumDrive(pidX.getOutput(robotVelocity.getX(), idealX), pidY.getOutput(robotVelocity.getY(), idealY),0); // y = cos, x = sin
+        }
     }
 
     private void cameraInit() {
