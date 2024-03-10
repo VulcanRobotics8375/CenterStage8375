57e91975c756930065b882f153de782eae97e246
SheiakahEye123 <86978045+SheiakahEye123@users.noreply.github.com>
2024-03-10 12:11:21 -0700

    simple centripedal+ringbufferimplementaiont

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/motion/followers/centripedal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotcorelib/motion/followers/centripedal.java
@@ -9,11 +9,15 @@ import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 import org.apache.commons.math3.util.FastMath;
 import org.firstinspires.ftc.teamcode.robotcorelib.math.geometry.Vector;
 
+import java.util.ArrayList;
+
 public class centripedal {
 
     ElapsedTime time = new ElapsedTime();
     Pose2d previous = new Pose2d(0,0);
-    Double lastDerivative = 0.0;
+    Double lastDerivative = 1.0;
+
+    ArrayList<Vector> buffa = new ArrayList<>();
 
 
 
@@ -26,7 +30,7 @@ public class centripedal {
         return Math.sqrt(Math.pow(Math.pow(derivative, 2) + 1, 3)) / secondDerivative;
     }
 
-    public double getParametricCurvature(double t ) {
+    public double auto(double t) {
         PolynomialSplineFunction xD = xSpline.polynomialSplineDerivative();
         PolynomialSplineFunction yD = ySpline.polynomialSplineDerivative();
 
@@ -40,6 +44,26 @@ public class centripedal {
         return 1/r;
 //       it is also important to understand why this works with a circle given the example by wolfpack movement breakdown
 //        https://en.wikipedia.org/wiki/Radius_of_curvature for a really good demonstration as well
+    }
+
+    public double tele(Vector pose){
+        buffa.add(pose);
+        if (buffa.size() < 3) {
+            return 0.0;
+        }
+        Vector pose1 = buffa.get(0);
+        Vector pose2 = buffa.get(1);
+        Vector pose3 = buffa.get(2);
+        buffa.remove(0);
+        buffa.remove(1);
+        buffa.remove(2);
+
+//        derive by x, dx/dx = 1, dy/dx is only thing we care about
+        double dy = (pose1.y-pose2.y)/(pose1.x-pose2.x);
+        double dy2 = dy-lastDerivative/(pose1.x-pose2.x);
+        lastDerivative = dy;
+        double r = (FastMath.sqrt(FastMath.pow((1+dy),3)))/dy2;
+        return 1/r;
     }
 
 
